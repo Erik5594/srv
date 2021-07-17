7720940f9309c0c0251ab54617087addb1f0756e
erik-qo <erik.derick74@gmail.com>
2021-07-17 15:15:22 -0300

    Alterando o modelo de chamada dos serviços para Handler e Router

---
+++ src/main/java/com/br/srv/handler/PautaHandler.java
@@ -0,0 +1,8 @@
+package com.br.srv.handler;
+
+/**
+ * @author erik_
+ * Data Criacao: 17/07/2021 - 13:59
+ */
+public class PautaHandler {
+}
---
+++ src/main/java/com/br/srv/router/PautaRouter.java
@@ -0,0 +1,8 @@
+package com.br.srv.router;
+
+/**
+ * @author erik_
+ * Data Criacao: 17/07/2021 - 14:02
+ */
+public class PautaRouter {
+}
